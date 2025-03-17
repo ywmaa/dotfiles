260cf64e05e7e452683706908bb433adfef515d2
ywmaa <ywmaa.personal@gmail.com>
2025-03-17 05:02:12 +0200

    add zsh.nix

---
+++ nix-config/zsh.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ...}:
+
+{
+  # ZSH
+  programs.zsh = {
+   # Your zsh config
+   enable = true;
+   autosuggestions.enable = true;
+   syntaxHighlighting.enable = true;
+   ohMyZsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "agnoster";
+    };
+  };
+  users.defaultUserShell = pkgs.zsh;
+  environment.shells = with pkgs; [ zsh ];
+}
